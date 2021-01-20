3afcfa4a63f232203c6cfea2f76e18aa96842676
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-20 15:08:27 -0300

    add styles no header

--- dogs/src/Components/Header.jsx
+++ dogs/src/Components/Header.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import styles from './Header.module.css'
-import {Link} from 'react-router-dom'
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login/ criar</Link>
-
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">Login/ criar</Link>
       </nav>
-    </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
