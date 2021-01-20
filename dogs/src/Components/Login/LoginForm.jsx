2d18905a759b27b3fb9a419801f73693393543d0
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-20 18:22:30 -0300

    add file api para definir endpoints da api

--- dogs/src/Components/Login/LoginForm.jsx
+++ dogs/src/Components/Login/LoginForm.jsx
@@ -3,27 +3,25 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import { TOKEN_POST } from "../../api";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const handleSubmit = (event) => {
+   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(),
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => console.log(json));
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      const response = await fetch(url,options);
+      const json = await response.json();
+      console.log(json);
+
     }
   };
 
