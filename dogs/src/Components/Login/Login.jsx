efdb421d956467c8b1e1fb2e24e46ed401918207
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-21 19:02:03 -0300

    add styles component Login

--- dogs/src/Components/Login/Login.jsx
+++ dogs/src/Components/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './Login.module.css';
 import {Routes, Route, Navigate} from 'react-router-dom';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
@@ -12,14 +13,16 @@ const Login = () => {
   if(login === true) <Navigate to="/conta"/>
 
   return (
-    <div>
+    <section className={styles.login} >
+      <div className={styles.forms}>
       <Routes>
         <Route path="/" element={<LoginForm/>}/>
         <Route path="criar" element={<LoginCreate/>}/>
         <Route path="perdeu" element={<LoginPasswordLost/>}/>
         <Route path="resetar" element={<LoginPasswordReset/>}/>
       </Routes>
-    </div>
+      </div>
+    </section>
   )
 }
 
