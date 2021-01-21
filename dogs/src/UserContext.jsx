b5d16085a2733e614e65d65787a29149a408fa72
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-21 15:47:48 -0300

    Add funçao que valida o token armazenado no localstorege

--- dogs/src/UserContext.jsx
+++ dogs/src/UserContext.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TOKEN_POST, USER_GET } from "./api";
+import { TOKEN_POST, USER_GET, TOKEN_VALIDATE_POST } from "./api";
 
 export const UserContext = React.createContext();
 
@@ -8,6 +8,19 @@ export const UserStorage = ({children}) => {
     const [login, setLogin] = React.useState(null);
     const [loading, setLoading]= React.useState(false);
     const [error,setError]= React.useState(null);
+
+    React.useEffect(()=>{
+        async function autoLogin(){
+            const token = window.localStorage.getItem('token');
+            if(token){
+             const {url, options} = TOKEN_VALIDATE_POST(token);
+             const response = await fetch(url, options);
+             const json = await response.json();   
+             console.log(json);
+           }
+        }   
+        autoLogin();
+    },[]);
 
     async function getUser(token){
         const {url,options} = USER_GET(token);
