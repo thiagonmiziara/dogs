68d13b89c9d21719bac476eaf246bca7f297d7f2
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-23 17:01:20 -0300

    Add menuMobile

--- dogs/src/Hooks/useMedia.jsx
+++ dogs/src/Hooks/useMedia.jsx
@@ -8,6 +8,7 @@ const useMedia = (media) => {
       const { matches } = window.matchMedia(media);
       setMatch(matches);
     }
+    changeMatch();
     window.addEventListener("resize", changeMatch);
     return () => {
       window.removeEventListener("resize", changeMatch);
