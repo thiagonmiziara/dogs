16a5f1c5351887862cae9480d6876f01f3f7217a
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-23 18:51:28 -0300

    Add metodo post na api  para usephotpost

--- dogs/src/Components/User/UserPhotoPost.jsx
+++ dogs/src/Components/User/UserPhotoPost.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import styles from "./UserPhotoPost.module.css";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
 
-function UserPhotoPost() {
+const UserPhotoPost = () => {
+  const nome = useForm();
+  const peso = useForm("number");
+  const idade = useForm("number");
+  const [img,setImg] = React.useState({});
+  const {data,error,loading, request} = useFetch();
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('img', img.raw);
+    formData.append('nome', nome.value);
+    formData.append('peso', peso.value);
+    formData.append('idade', idade.value);
+    
+    request(url,options);
+
+    
+
+  }
+
+  function handleImgChange({target}) {
+    setImg({
+      raw: target.files[0],
+    });
+  }
+
   return (
-    <div>
-      Foto
-    </div>
-  )
-}
+    <section className={`${styles.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="text" name="nome" />
+        <Input label="Peso" type="text" name="peso" />
+        <Input label="Idade" type="text" name="idade" />
+        <input type="file" name="img" id="img" onChange={handleImgChange} />
+        <Button>Enviar</Button>
+      </form>
+    </section>
+  );
+};
 
 export default UserPhotoPost;
