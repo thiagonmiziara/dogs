c996f37cc21e0c36e8773b915441723a1ac3e8fe
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-23 19:19:14 -0300

    add useNavigate para redirecionar apos o POST

--- dogs/src/Components/User/UserPhotoPost.jsx
+++ dogs/src/Components/User/UserPhotoPost.jsx
@@ -4,7 +4,9 @@ import useForm from "../../Hooks/useForm";
 import useFetch from "../../Hooks/useFetch";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import Error from "../Helpers/Error";
 import { PHOTO_POST } from "../../api";
+import { useNavigate } from "react-router-dom";
 
 const UserPhotoPost = () => {
   const nome = useForm();
@@ -12,6 +14,11 @@ const UserPhotoPost = () => {
   const idade = useForm("number");
   const [img, setImg] = React.useState({});
   const { data, error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (data) navigate("/conta");
+  }, [data, navigate]);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -39,8 +46,19 @@ const UserPhotoPost = () => {
         <Input label="Nome" type="text" name="nome" {...nome} />
         <Input label="Peso" type="number" name="peso" {...peso} />
         <Input label="Idade" type="number" name="idade" {...idade} />
-        <input className={styles.file} type="file" name="img" id="img" onChange={handleImgChange} />
-        <Button>Enviar</Button>
+        <input
+          className={styles.file}
+          type="file"
+          name="img"
+          id="img"
+          onChange={handleImgChange}
+        />
+        {loading ? (
+          <Button disabled>Enviando...</Button>
+        ) : (
+          <Button>Enviar</Button>
+        )}
+        <Error error={error} />
       </form>
       <div>
         {img.preview && (
