d3f5e5965394ba8405f6c2c2eff30ccad10dcab4
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-23 14:37:03 -0300

    Add component Helper ProtctedRoute

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -7,6 +7,7 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
 import User from "./Components/User/User";
+import ProtectedRoute from "./Components/Helpers/ProtectedRoute";
 
 const App = () => {
   return (
@@ -17,7 +18,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
-            <Route path="conta/*" element={<User />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
--- dogs/src/UserContext.jsx
+++ dogs/src/UserContext.jsx
@@ -70,6 +70,8 @@ export const UserStorage = ({ children }) => {
         } finally {
           setLoading(false);
         }
+      }else{
+        setLogin(false);
       }
     }
     autoLogin();
