1891d71ff7511759d7861b338957f2ca91e89d99
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-23 15:43:56 -0300

    add component UserHeader e os styles

--- dogs/src/Components/User/UserHeader.jsx
+++ dogs/src/Components/User/UserHeader.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import UserHeaderNav from './UserHeaderNav';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import styles from "./UserHeader.module.css";
+import UserHeaderNav from "./UserHeaderNav";
 
 const UserHeader = () => {
+  const [title, setTitle] = React.useState("");
+  const location = useLocation();
+
+  React.useEffect(() => {
+    const { pathname } = location;
+    switch (pathname) {
+      case "/conta/estatisticas":
+        setTitle("Estatísticas");
+        break;
+      case "/conta/postar":
+        setTitle("Poste Sua Foto");
+        break;
+      default:
+        setTitle("Minha Conta");
+    }
+  }, [location]);
+
   return (
-    <header>
-      <h1 className="title">Titulo</h1>
-      <UserHeaderNav/>
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
+      <UserHeaderNav />
     </header>
-  )
-}
+  );
+};
 
 export default UserHeader;
