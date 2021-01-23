f1a14d1b9c414f1af8e0e5d380465fc091ccf6eb
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-23 14:28:27 -0300

    Add component User

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { UserStorage } from "./UserContext";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
-import { UserStorage } from "./UserContext";
+import User from "./Components/User/User";
 
 const App = () => {
   return (
@@ -15,7 +16,8 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
---
+++ dogs/src/Components/User/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>
+      Usuário
+    </div>
+  )
+}
+
+export default User;
