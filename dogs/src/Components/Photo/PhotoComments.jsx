456072ec1463dcb3877dba8928e30747d7c38c93
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-30 20:09:26 -0300

    add estilo aos comentarios

--- dogs/src/Components/Photo/PhotoComments.jsx
+++ dogs/src/Components/Photo/PhotoComments.jsx
@@ -5,11 +5,18 @@ import styles from "./PhotoComments.module.css";
 
 const PhotoComments = (props) => {
   const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef(null); 
   const { login } = React.useContext(UserContext);
+
+React.useEffect(()=>{
+
+  commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+
+},[comments]);
 
   return (
     <>
-      <ul className={styles.comments}>
+      <ul ref={commentsSection} className={styles.comments}>
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
             <b>{comment.comment_author}: </b>
--- dogs/src/Components/Photo/PhotoCommentsForm.jsx
+++ dogs/src/Components/Photo/PhotoCommentsForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./PhotoCommentsForm.module.css"
 import { COMMENT_POST } from "../../api";
 import { ReactComponent as Enviar } from "../../Assets/enviar.svg";
 import Error from "../Helpers/Error";
@@ -21,15 +22,16 @@ const PhotoCommentsForm = ({id, setComments}) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
       <Error error={error}/>
