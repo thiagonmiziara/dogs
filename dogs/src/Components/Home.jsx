5e3bfe8ac1308db30049a4cc7331365c21379bca
thiagonmiziara <thiagonmiziara@gmail.com>
2021-02-01 16:30:22 -0300

    add logica de animaçao e estilo no component Login

--- dogs/src/Components/Home.jsx
+++ dogs/src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Feed from '../Components/Feed/Feed';
+import Loading from './Helpers/Loading';
 
 const Home = () => {
   return (
     <section className="container mainContainer">
       <Feed/>
+      
     </section>
       
     
