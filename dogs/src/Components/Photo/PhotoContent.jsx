75e59b854a856735ef03762da70efe948e057309
thiagonmiziara <thiagonmiziara@gmail.com>
2021-02-01 14:52:35 -0300

    Add component PhotoDelete para deletar fotos

--- dogs/src/Components/Photo/PhotoContent.jsx
+++ dogs/src/Components/Photo/PhotoContent.jsx
@@ -1,38 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 import PhotoComments from "./PhotoComments";
 import styles from "./PhotoContent.module.css";
+import PhotoDelete from "./PhotoDelete";
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
+  const { photo, comments } = data;
 
-  const {photo,comments} = data;
-
-  return <div className={styles.photo}>
-    <div className={styles.img}>
-      <img src={photo.src} alt={photo.title}/>
-    </div>
-    <div className={styles.details}>
-      <div>
-        <p className={styles.author}>
-          <Link to={`/perfil/${photo.author}`}>
-            @{photo.author}
-          </Link>
-          <span className={styles.preview}>{photo.acessos}</span>
-        </p>
-        <h1 className="title">
-            <Link to={`/foto/${photo.id}`}>
-              {photo.title}
-            </Link>
-        </h1>
-        <ul className={styles.attributes}>
-          <li>{photo.peso}Kg</li>
-          <li>{photo.idade} anos</li>
-
-        </ul>
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
       </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+            <span className={styles.preview}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso}Kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
+        </div>
+      </div>
+      <PhotoComments id={photo.id} comments={comments} />
     </div>
-    <PhotoComments id={photo.id} comments={comments}/>
-  </div>;
+  );
 };
 
 export default PhotoContent;
---
+++ dogs/src/Components/Photo/PhotoDelete.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { PHOTO_DELETE } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+import styles from "./PhotoDelete.module.css";
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  async function handleClick() {
+    const confirm = window.confirm("Tem certeza que deseja DELETAR?");
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) {
+        window.location.reload();
+      }
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Deletar
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;
