d650b05b94b96a88236535344fb490744a98eb96
thiagonmiziara <thiagonmiziara@gmail.com>
2021-02-02 15:22:38 -0300

    add proptypes no component feed

--- dogs/src/Components/Feed/Feed.jsx
+++ dogs/src/Components/Feed/Feed.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
+import PropTypes from "prop-types";
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
@@ -51,4 +52,14 @@ const Feed = ({ user }) => {
   );
 };
 
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
+};
 export default Feed;
