4c491b6258b96eb724204504a4324c1f1dfee7bd
thiagonmiziara <thiagonmiziara@gmail.com>
2021-02-02 15:57:50 -0300

    add logica de resetar senha no component loginpasswordlost

--- dogs/src/api.js
+++ dogs/src/api.js
@@ -108,3 +108,16 @@ export function PHOTO_DELETE(id) {
         },
     };
 }
+
+export function PASSWORD_LOST(body) {
+    return {
+        url: API_URL + "/api/password/lost",
+        options: {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        },
+    };
+}
